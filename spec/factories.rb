4bd2aad6869a0b45f89e871416042736a11be766
belozi <robert.belozi.lee@gmail.com>
2014-04-12 11:01:28 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name					"Robert Belozi Lee"
+		email					"robert.belozi.lee@gmail.com"
+		password 				"Krishna07"
+		password_confirmation	"Krishna07"
+	end
+end
